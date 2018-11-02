76b943aa110a873de56d2c8da54bcb95b7485486
geirgie25 <43558674+geirgie25@users.noreply.github.com>
2018-11-02 17:08:11 -0500

    initial
    howdy

---
+++ Assets/ScriptsR/Player/Projectiles/StraightShotScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightShotScript : MonoBehaviour {
+
+	[SerializeField]
+    float projectileSpeed, deathTimer;
+    Vector3 velocity;
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        projectileSpeed *= 1500f;
+        Debug.Log(transform.right);
+        velocity = new Vector3(transform.right.x * projectileSpeed, transform.right.y * projectileSpeed, transform.right.z);
+        Debug.Log(velocity);
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(velocity);
+        StartCoroutine("killSelf");
+    }
+
+    IEnumerator killSelf()
+    {
+        yield return new WaitForSeconds(deathTimer);
+        Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        //kys if ur reading this
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Obstacles"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
